f77b462419b890d7f5ea05d456407d5161e5505d
Victor TheLuven <victor.venuleth@reseau.eseo.fr>
2024-04-14 22:44:38 +0200

    Login Singin page

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './components/vues/Home.vue';
 import Verify from "./components/vues/Verify.vue";
+import Login from "./components/vues/Login.vue";
 
 const routes = [
     { path: '/home', component: Home },
-    { path: '/verify', component: Verify }
+    { path: '/verify', component: Verify },
+    { path: '/login', component: Login }
 
 ];
 
